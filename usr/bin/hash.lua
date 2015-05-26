78f1b0de39f019d34a28d3ad122f10b2bb42eb91
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2015-05-26 13:57:46 -0300

    Added more algorithms!

--- usr/bin/hash.lua
+++ usr/bin/hash.lua
@@ -26,36 +26,49 @@ function main(...)
   local args = {}
   local isfi;
 
-  local hash = (run.require 'hash').sha256
+  local hash = (run.require 'hash')
+  local algo;
 
-  for opt, arg in (run.require 'posix').getopt('hvF', ...) do
+  for opt, arg in (run.require 'posix').getopt('hvFA:', ...) do
     if opt == false and #arg ~= 0 then
       table.insert(args, arg)
     elseif opt == 'F' then isfi = true
     elseif opt == 'h' then (run.require 'info').usage('sha', 'calculate secure hashing algorithm 256 sum', '-F <file[file]...>/ <string[string]...>', {
       h = 'print help',
       v = 'print version',
-      F = 'toggle file mode'
+      F = 'toggle file mode',
+      A = 'specify hashing algorithm.'
     }) return
-    elseif opt == 'v' then print('sha256 version 1') return end
+    elseif opt == 'v' then print('sha256 version 1') return
+    elseif opt == 'A' then algo = arg end
   end
 
+  if not algo then
+    hash = hash.sha256
+    algo = 'sha256'
+  else
+    hash = hash[algo] or hash.sha256
+    if hash == (run.require 'hash').sha256 then
+      algo = 'sha256'
+      printError('Unknown algorithm ' .. algo)
+    end
+  end
 
   if isfi then
     for i = 1, #args do
       if fs.exists(shell.resolve(args[i])) and not fs.isDir(shell.resolve(args[i])) then
         local x = fs.open(shell.resolve(args[i]), 'r')
-        print(shell.resolve(args[i]) == '/' and '/' or '/' .. shell.resolve(args[i]).. ': ' .. hash(x.readAll()))
+        print(algo .. ' hash of: ' .. shell.resolve(args[i]) == '/' and '/' or '/' .. shell.resolve(args[i]).. ': ' .. hash(x.readAll()))
         x.close()
       elseif fs.isDir(shell.resolve(args[i])) then
-        print(shell.resolve(args[i]) == '/' and '/' or '/' .. shell.resolve(args[i]) .. ': is a directory.')
+        print(algo .. ' hash of: ' .. shell.resolve(args[i]) == '/' and '/' or '/' .. shell.resolve(args[i]) .. ': is a directory.')
       elseif not fs.exists(shell.resolve(args[i])) then
-        print(shell.resolve(args[i]) == '/' and '/' or '/' .. shell.resolve(args[i])  .. ': does not exist.')
+        print(algo .. ' hash of: ' .. shell.resolve(args[i]) == '/' and '/' or '/' .. shell.resolve(args[i])  .. ': does not exist.')
       end
     end
   else
     for i = 1, #args do
-      print((args[i]) .. ': ' .. hash(args[i]))
+      print(algo .. ' hash of: ' .. (args[i]) .. ': ' .. hash(args[i]))
     end
   end
 
